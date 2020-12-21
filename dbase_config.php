09895b65ebf5d0842561cd8c56cba38d5abc3a80
Joao Kawase <36015743+jkawase1@users.noreply.github.com>
2020-12-21 15:52:53 -0500

    Small change

--- dbase_config.php
+++ dbase_config.php
@@ -1,6 +1,6 @@
 <?php
         $dbhost = "dbase.cs.jhu.edu"; // database server name
         $dbuser = "20fa_jkawase1"; // dbase username
-        $dbpass = "9Thcf39vTL"; // dbase password
+        $dbpass = ; // dbase password
         $dbname = "20fa_jkawase1_db"; // name of db 
 ?>
